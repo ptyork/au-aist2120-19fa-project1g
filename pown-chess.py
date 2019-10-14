2810d29e69655ac9367e5e5cd27eb3ce798094e8
Paul York <paul@yorkfamily.com>
2019-10-14 17:03:01 -0400

    Initial commit

---
+++ pown-chess.py
@@ -0,0 +1,29 @@
+# Game board is a "dictionary" of strings.
+# Keys will be to board "coordinates" (col+row).
+# Values will be '', 'w' or 'b'.
+board = {}
+
+# Write code to initialize the dictionary here
+# (e.g., board['A1'] = .......)
+
+# write a function that shows the board according to the instructions
+def showboard():
+    # do your thing here
+    return
+
+
+# write a function returns the COUNT of pieces for the given color
+# that can still move
+def countmoves(color):
+    # do your thing here
+    return 0  # change this to return the actual count
+
+
+# Add any additional functions if you want them below here
+
+
+
+# And finally, do your actual gameplay (the code that runs
+# when you execute your script) below here...while True:
+# is likely the right place to begin.
+
